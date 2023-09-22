3e0e2bd662d85559c7972e5db4ba937b5e933de2
황웅희 <dndgml99@freeive.com>
2023-09-22 09:32:03 +0900

    build test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const path = require('path'); 
-
 const nextConfig = {
   reactStrictMode: false,
+  basePath: "/newPortfolio",
+  output: 'export',
   sassOptions: {
     includePaths: [path.join(__dirname, 'scss')],
     prependData: `@import "./src/app/scss/common/variable.module.scss"; @import "./src/app/scss/common/common.scss";`, // prependData 옵션 추가
