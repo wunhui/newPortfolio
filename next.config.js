080fbf155cbf6afd611acfb7e3481b48292afecb
황웅희 <dndgml99@freeive.com>
2023-09-22 10:05:59 +0900

    build test2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 
+const isProduction = process.env.NODE_ENV === 'production';
 const path = require('path'); 
 const nextConfig = {
   reactStrictMode: false,
-  basePath: "/newPortfolio",
   output: 'export',
+  basePath: '/newPortfolio',
+  assetPrefix: isProduction ? '/premium-page' : '',
   sassOptions: {
     includePaths: [path.join(__dirname, 'scss')],
     prependData: `@import "./src/app/scss/common/variable.module.scss"; @import "./src/app/scss/common/common.scss";`, // prependData 옵션 추가
